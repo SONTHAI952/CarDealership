eaba0a16775dea827aeaeee440fe56381fe9acab
Sonthai <nogthaison@gmail.com>
2022-06-14 15:45:59 +0700

    recreate .Web project

---
+++ CarDealerships.Web/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CarDealership.DataAcess.Models;
+using System.Data.SqlClient;
+using CarDealership.DataAcess.Repositories;
+
+
+namespace CarDealership.Web.Controllers
+{
+    public class CarController : Controller
+    {
+        private ICarRepository _carRepository;
+
+
+
+
+        [HttpGet]
+        public IActionResult Index(string query)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "server = localhost; port = 3306; user = < root >; password = < null >; database = < db00 > ";
+            _carRepository = new CarRepository(conn);
+            var cars = _carRepository.GetCars(query);
+            return View(cars);
+
+        }
+
+    }
+}
